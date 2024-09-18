5956ec34d776710ecde55cc4faac4026d58c62ac
OgurecA <acpwork044@gmail.com>
2024-09-18 17:15:17 +0400

    k

--- src/Components/Tasks/Tasks.jsx
+++ src/Components/Tasks/Tasks.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Tasks.css'; // Импортируем CSS для стилизации
 
 const Tasks = () => {
