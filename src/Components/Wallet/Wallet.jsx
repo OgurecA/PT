e9c3f691ae11844beac4cf7d16e173ff16521852
OgurecA <acpwork044@gmail.com>
2024-09-19 12:37:29 +0400

    k

--- src/Components/Wallet/Wallet.jsx
+++ src/Components/Wallet/Wallet.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import './Wallet.css'; // Импортируем CSS для стилизации
-import WalletIcon from '../Photo/Wallet.png'
+import WalletIcon from '../Photo/Wallet.png';
 import { TonConnect } from "@tonconnect/sdk"; // Импортируем TonConnect для работы с кошельком
 
 const Wallet = () => {
   const [walletAddress, setWalletAddress] = useState(null); // Состояние для хранения адреса кошелька
+  const [isImageVisible, setImageVisible] = useState(true); // Состояние для управления видимостью изображения
   const tonConnect = new TonConnect();
 
   const connectWallet = async () => {
@@ -16,14 +17,20 @@ const Wallet = () => {
     }
   };
 
+  // Функция для проверки, работает ли кнопка (скрывает изображение при нажатии)
+  const handleButtonClick = () => {
+    setImageVisible(false);
+  };
+
   return (
     <div className="wallet">
+      {isImageVisible && <img src={WalletIcon} alt="Wallet Icon" className="wallet-image" />}
       {walletAddress ? (
         <div className="wallet-address">
           <p>Wallet: {walletAddress}</p>
         </div>
       ) : (
-        <button className="wallet-button" onClick={connectWallet}>Connect Wallet</button>
+        <button className="wallet-button" onClick={handleButtonClick}>Connect Wallet</button>
       )}
     </div>
   );
