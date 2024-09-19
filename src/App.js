ae46802e4106c9874ae6be829eb7afe2912b1df3
OgurecA <acpwork044@gmail.com>
2024-09-20 01:43:10 +0400

    k

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Coin from './Components/Coin/Coin';
