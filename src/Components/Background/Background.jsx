bbb3bd49079f70236d4ef68a6237ce861b57ff6e
OgurecA <acpwork044@gmail.com>
2024-09-27 14:31:04 +0400

    k

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import './BackgroundContainer.css'; // Импортируем стили
+import './BackgroundContainer.css';
+import backgroundImage from '../Photo/Background2.png';
 
 const BackgroundContainer = () => {
   return (
-    <div className="background-container"></div>
+    <div
+      className="background-container"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    ></div>
   );
 };
 
