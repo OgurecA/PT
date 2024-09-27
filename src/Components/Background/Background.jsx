9d04a5a21c1db5d0603e03098d44bf5045bdad84
OgurecA <acpwork044@gmail.com>
2024-09-27 14:24:38 +0400

    k

---
+++ src/Components/Background/Background.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './BackgroundContainer.css'; // Импортируем стили
+
+const BackgroundContainer = () => {
+  return (
+    <div className="background-container"></div>
+  );
+};
+
+export default BackgroundContainer;
