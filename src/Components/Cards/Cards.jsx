c0765644378e8c1769b59af3345c2fb374443813
OgurecA <acpwork044@gmail.com>
2024-10-16 18:01:19 +0400

    k

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -16,19 +16,19 @@ const Card = () => {
   return (
     <div className="card-container">
       <div
-        className={`card card1 ${selectedCard === null ? '' : selectedCard === 1 ? 'chosen' : 'notchosen'} ${cardRotate === 1 ? 'flip-scale-up-diag-1' : ''}`}
+        className={`card card1 ${selectedCard === null ? '' : selectedCard === 1 ? 'chosen' : 'notchosen'} ${cardRotate === 1 ? 'flip-scale-up-ver' : ''}`}
         onClick={() => handleCardClick(1)}
       >
         <img src={Rubashka} alt="Card 1" className="card-image" />
       </div>
       <div
-        className={`card card2 ${selectedCard === null ? '' : selectedCard === 2 ? 'chosen' : 'notchosen'} ${cardRotate === 2 ? 'flip-scale-up-diag-1' : ''}`}
+        className={`card card2 ${selectedCard === null ? '' : selectedCard === 2 ? 'chosen' : 'notchosen'} ${cardRotate === 2 ? 'flip-scale-up-ver' : ''}`}
         onClick={() => handleCardClick(2)}
       >
         <img src={Rubashka} alt="Card 2" className="card-image" />
       </div>
       <div
-        className={`card card3 ${selectedCard === null ? '' : selectedCard === 3 ? 'chosen' : 'notchosen'} ${cardRotate === 3 ? 'flip-scale-up-diag-1' : ''}`}
+        className={`card card3 ${selectedCard === null ? '' : selectedCard === 3 ? 'chosen' : 'notchosen'} ${cardRotate === 3 ? 'flip-scale-up-ver' : ''}`}
         onClick={() => handleCardClick(3)}
       >
         <img src={Rubashka} alt="Card 3" className="card-image" />
